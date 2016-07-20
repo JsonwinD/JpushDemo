35c765359e792c707c7e095c628569042b3a5c27
JsonwinD <237290563@qq.com>
2016-07-20 10:03:40 +0800

    First Commit
    极光推送测试项目

---
+++ JpushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JpushDemo
+//
+//  Created by Jaqen on 16/7/20.
+//  Copyright © 2016年 lairong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JpushDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JpushDemo
+//
+//  Created by Jaqen on 16/7/20.
+//  Copyright © 2016年 lairong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
